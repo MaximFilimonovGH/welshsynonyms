e2bbe57c91bc492753dcd983ce69c984f802df8b
Maxim Filimonov <max.filimonov@gmail.com>
2020-06-29 13:03:10 +0100

    local database instaed of atlas

--- backend/database/db.config.js
+++ backend/database/db.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: 'mongodb+srv://wordNetUser:XmKY15TZiHEGKYOp@cluster0-xrhyi.gcp.mongodb.net/wordNetWelsh?retryWrites=true&w=majority'
+    //url: 'mongodb+srv://wordNetUser:XmKY15TZiHEGKYOp@cluster0-xrhyi.gcp.mongodb.net/wordNetWelsh?retryWrites=true&w=majority'
+    url: 'mongodb://WordNetUser:XmKY15TZiHEGKYOp@localhost:27017/wordNetWelsh'
   };

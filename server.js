e932e0a47e07a0f2a505b2f9c9e9a0a194e5fdcd
Maxim Filimonov <max.filimonov@gmail.com>
2020-07-23 15:07:33 +0100

    back to port 8082 on server

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.all("/*", function(req, res, next) {
 });
 
 //set port, listen for requests
-const PORT = process.env.PORT || 4202;
+const PORT = process.env.PORT || 8082;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });

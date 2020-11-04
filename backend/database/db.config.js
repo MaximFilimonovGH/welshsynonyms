bae28c1f08d42b4dd65468965b7c3066cb35fa35
Maxim Filimonov <max.filimonov@gmail.com>
2020-11-04 13:07:14 +0000

    remove old urls

--- backend/database/db.config.js
+++ backend/database/db.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-    //url: 'mongodb+srv://wordNetUser:XmKY15TZiHEGKYOp@cluster0-xrhyi.gcp.mongodb.net/wordNetWelsh?retryWrites=true&w=majority'
-    urlSynonymsGame: 'mongodb://synonymsGameUser:w5u72fhGrPsZ9cag@localhost:27017/synonymsGame',
+    //urlSynonymsGame: 'mongodb://synonymsGameUser:w5u72fhGrPsZ9cag@localhost:27017/synonymsGame',
     urlWelshWords: 'mongodb://welshWordsListsUser:75VJHwYtdHfBFWxw@localhost:27017/welshWordsLists',
     urlWordNet: 'mongodb://wordNetUser:XmKY15TZiHEGKYOp@localhost:27017/wordNetWelsh'
   };

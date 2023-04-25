1d41bc6bf8a85029c36facde259ab50c7269eb7c
Maxim Filimonov <max.filimonov@gmail.com>
2023-04-25 14:03:19 +0100

    removed last single db implementations

--- server.js
+++ server.js
@@ -10,21 +10,6 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 
-//connect to single synonymsGame database
-// const db = require("./backend/models");
-// db.mongoose
-//     .connect(db.url, {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true
-//     })
-//     .then(() => {
-//         console.log("Connected to the database");
-//     })
-//     .catch(err => {
-//         console.log("Cannot connect to the database", err);
-//         process.exit();
-//     });
-
 //parse requests of content-type - application/json
 app.use(express.json());
 

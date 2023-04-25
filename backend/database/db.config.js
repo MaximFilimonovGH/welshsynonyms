da4ef5223e405e49d1d930cdfceada86f349f8a5
Maxim Filimonov <max.filimonov@gmail.com>
2023-04-25 12:56:58 +0100

    mongodb config in env

--- backend/database/db.config.js
+++ backend/database/db.config.js
@@ -1,4 +1,11 @@
+// read environment variables from .env file
+require('dotenv').config({ path: './.env'})
+const welshWordsListsUser = process.env.WELSHWORDSLISTS_USER;
+const welshWordsListsPassword = process.env.WELSHWORDSLISTS_PASSWORD;
+const wordNetUser = process.env.WORDNET_USER;
+const wordNetPassword = process.env.WORDNET_PASSWORD;
+
 module.exports = {
-    urlWelshWords: 'mongodb://welshWordsListsUser:75VJHwYtdHfBFWxw@localhost:27017/welshWordsLists',
-    urlWordNet: 'mongodb://wordNetUser:XmKY15TZiHEGKYOp@localhost:27017/wordNetWelsh'
+    urlWelshWords: `mongodb://${welshWordsListsUser}:${welshWordsListsPassword}@localhost:27017/welshWordsLists`,
+    urlWordNet: `mongodb://${wordNetUser}:${wordNetPassword}@localhost:27017/wordNetWelsh`
   };
